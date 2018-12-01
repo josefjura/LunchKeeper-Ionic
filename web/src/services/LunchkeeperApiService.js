e1eb8fce2ed86b447241487da57fc9106412c301
Josef Jura <josef.jura@hotmail.com>
2018-12-01 18:25:50 +0100

    Fixed errors I caused, because I am an idiot

--- web/src/services/LunchkeeperApiService.js
+++ web/src/services/LunchkeeperApiService.js
@@ -4,16 +4,34 @@ export const API_URL = "https://lunchkeeper-api.herokuapp.com/api"
 
 
 export async function search(query) {
-    var result = await axios.get(`${API_URL}/lunch?q=${query}`);
-    return result.data;
+    try {
+        var result = await axios.get(`${API_URL}/lunch?q=${query}`);
+        return result.data.restaurants;
+    }
+    catch (err) {
+        console.err(err);
+        return [];
+    }
 }
 
 export async function getDetails(id, source) {
-    var result = await axios.get(`${API_URL}/lunch/${source}/${id}`);    
-    return result.data;
+    try {
+        var result = await axios.get(`${API_URL}/lunch/${source}/${id}`);
+        return result.data;
+    }
+    catch (err) {
+        console.err(err);
+        return {};
+    }
 }
 
 export async function getMenu(id, source) {
-    var result = await axios.get(`${API_URL}/lunch/${source}/${id}/menu`);
-    return result.data;
+    try {
+        var result = await axios.get(`${API_URL}/lunch/${source}/${id}/menu`);
+        return result.data;
+    }
+    catch (err) {
+        console.err(err);
+        return {};
+    }
 }
