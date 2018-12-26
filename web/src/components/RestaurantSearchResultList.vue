101f85db2f724316e120df50cdec966861aafe4e
Josef Jura <josef.jura@hotmail.com>
2018-12-26 16:47:28 +0100

    Changed search result interface

--- web/src/components/RestaurantSearchResultList.vue
+++ web/src/components/RestaurantSearchResultList.vue
@@ -1,36 +1,77 @@
 <template>
   <div>
     <ul v-if="!loading">
-      <RestaurantSearchResult v-for="result in results" :key="result.id" :name="result.name" :thumb="result.thumb" :id="result.id" :source="result.source" />
+      <!-- <RestaurantSearchResult v-for="result in results" :key="result.id" :name="result.name" :thumb="result.thumb" :id="result.id" :source="result.source" /> -->
+      <v-data-table
+        hide-headers
+        hide-actions
+        :headers="headers"
+        :items="results"
+        class="elevation-1"
+      >
+        <template slot="items" slot-scope="props">
+          <td class="source-type">
+            <source-icon :source="props.item.source" ></source-icon>
+          </td>
+          <td class="text-xs-right">{{ props.item.name }}</td>
+          <td class="background-paint">
+            <img :src="props.item.thumb || require('../assets/search_default_icon.jpg')">
+          </td>
+        </template>
+        <template slot="no-data">
+          <h2 class="text-xs-center">Search for restaurants you want to add to your menu list.</h2>
+        </template>
+      </v-data-table>
     </ul>
     <div v-if="loading" class="text-xs-center">
-      <v-progress-circular
-        :size="50"
-        color="primary"
-        indeterminate
-      ></v-progress-circular>
+      <v-progress-circular :size="50" color="primary" indeterminate></v-progress-circular>
     </div>
-    <div v-if="!loading && results.length == 0" class="text-xs-center">
-      <h2>Search for restaurants you want to add to your menu list.</h2>
-    </div>
+    <div v-if="!loading && results.length == 0" class="text-xs-center"></div>
   </div>
 </template>
 
 <script>
-import RestaurantSearchResult from "../components/RestaurantSearchResult";
+import SourceIcon from './SourceIcon' 
 
 export default {
   props: ["results", "loading"],
   data: () => ({
-
+    headers: [
+      { text: "Source", sortable: true },
+      { text: "Name", sortable: true, align: "left" },
+      { sortable: false, align: "right", width: "50px" }
+    ]
   }),
-  mounted() { },
-  methods: {
-
-  },
+  mounted() {},
+  methods: {},
   components: {
-    RestaurantSearchResult
+    SourceIcon
   }
 };
 </script>
+
+<style>
+
+.source-type img{
+  width:50px;
+  height:50px;
+  margin-top: 5px;
+}
+
+.background-paint {
+  width: 60px;
+}
+
+.background-paint img {
+  background-color: black;
+  width: 50px;
+  height: 50px;
+  margin-top: 5px;
+}
+.background-paint span {
+  font-size: 20px;
+  margin-right: 10px;
+}
+</style>
+
 
---
+++ web/src/components/SourceIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <img :src="this.src">
+</template>
+
+<script>
+
+export default {
+  props: ["source"],
+  computed: {
+    src: function() {
+      if (this.source == 0) {
+        return "https://b.zmtcdn.com/images/logo/square_zomato_logo_new-2.svg";
+      }
+      if (this.source == 1) {        
+        return require("../assets/search_default_icon.jpg");
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
